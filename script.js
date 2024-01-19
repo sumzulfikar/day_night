1c87feeef318a83095c23aed901967a4162158ed
sumzulfikar <sumaiya.zulfikar29@gmail.com>
2024-01-19 11:02:07 +0000

    functional greetings

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var themeSwitcher=document.getElementById("theme-switcher");
 
 var container=document.querySelector(".container")
 var mode="light";
-console.log(themeSwitcher);
+//console.log(themeSwitcher);
 
 themeSwitcher.addEventListener("change",function(){
 
@@ -24,3 +24,23 @@ var timemins=dayjs().format("mm");
 $("#mins").text(timemins);
 var timepart=dayjs().format("a");
 $("#part").text(timepart);
+//greeting based on hour of day
+$("#greeting-text").text(greetings);
+var greetingstxt="";
+function greetings(){
+if(timehrs<12){
+    greetingstxt="Morning!";
+    return greetingstxt;
+}
+else if(timehrs>=12&&timehrs<18){
+    greetingstxt="Afternoon!";
+    return greetingstxt;
+}
+else if(timehrs>=18){
+    greetingstxt="Night!";
+    return greetingstxt;
+}
+else
+return;
+
+}
